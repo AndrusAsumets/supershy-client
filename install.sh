fe664f70ce62c96607ee3fd2f01f44bf753675eb
Andrus Asumets <andrus.asumets@gmail.com>
2024-12-10 19:08:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ case $(uname -sm) in
 esac
 
 distro=$(( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1)
-case "$distro" in
+case $distro in
 	"*Ubuntu*") sudo apt install unzip expect -y ;;
 	"*Debian*") sudo apt install unzip expect -y ;;
 	"*Fedora*") sudo dnf install unzip expect -y ;;
