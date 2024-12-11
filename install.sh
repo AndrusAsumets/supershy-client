5fcbad81618c05207b3908946040040bd21f0d49
Andrus Asumets <andrus.asumets@gmail.com>
2024-12-11 15:10:13 +0200

    v0.2.22

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
-yum_cmd=$(type -p yum)
-dnf_cmd=$(type -p dnf)
-apt_cmd=$(type -p apt)
+user=$1
 
-if [[ ! -z $yum_cmd ]]; then
+# install dependencies
+if [[ ! -z $(type -p yum) ]]; then
     sudo yum install unzip expect -y
-elif [[ ! -z $dnf_cmd ]]; then
+elif [[ ! -z $(type -p dnf) ]]; then
     sudo dnf install unzip expect -y
-elif [[ ! -z $apt_cmd ]]; then
+elif [[ ! -z $(type -p apt) ]]; then
     sudo apt install unzip expect -y
+elif [[ ! -z $(type -p brew) ]]; then
+    sudo -u $user brew install unzip
+    sudo -u $user brew install expect
 else
     echo "Warning: Can't install packages as no package manager was found."
 fi
 
+# set platform target
 case $(uname -sm) in
 	"Darwin x86_64") target="supershy-macos-x86_64" ;;
 	"Darwin arm64") target="supershy-macos-arm64" ;;
@@ -19,15 +22,19 @@ case $(uname -sm) in
 	*) target="supershy-linux-x86_64" ;;
 esac
 
-user=$1
 version="$(curl -s https://version.supershy.org/)"
 uri="https://github.com/AndrusAsumets/supershy-client/releases/download/${version}/${target}.zip"
-tmp_dir="/tmp"
+app_id="org.supershy.supershyd"
+daemon="/etc/systemd/user/supershy-daemon.service"
 bin_dir="/usr/bin"
+if [[ $target == *"macos"* ]]; then
+    daemon="/Users/$user/Library/LaunchAgents/${app_id}.plist"
+    bin_dir="/usr/local/bin"
+fi
+tmp_dir="/tmp"
 zip="$tmp_dir/supershy.zip"
 tmp_exe="$tmp_dir/supershyd"
 exe="$bin_dir/supershyd"
-daemon="/etc/systemd/user/supershy-daemon.service"
 
 # remove old installation
 sudo rm -rf $exe
@@ -39,15 +46,16 @@ curl --fail --location --progress-bar --output "$zip" "$uri"
 unzip -d "$tmp_dir" -o "$zip"
 
 # move to binaries
-mv $tmp_exe $exe
+sudo mv $tmp_exe $exe
 
-# create daemon service
+sudo chown -R $user $exe
+
+# remove old daemon service
+rm -f $daemon
+
+# create daemon servicea
 case $target in
     *"linux"*)
-        # remove old daemon service
-        rm -f $daemon
-
-        # create new daemon service
         sudo echo '[Unit]' >> $daemon
         sudo echo 'Description=supershyd' >> $daemon
 
@@ -59,8 +67,31 @@ case $target in
         sudo echo 'WantedBy=default.target' >> $daemon
 
         # run supershy daemon in background
-	sudo -u $user XDG_RUNTIME_DIR="/run/user/$(id -u $user)" systemctl --user daemon-reload
+        sudo -u $user XDG_RUNTIME_DIR="/run/user/$(id -u $user)" systemctl --user daemon-reload
         sudo -u $user XDG_RUNTIME_DIR="/run/user/$(id -u $user)" systemctl --user enable supershy-daemon.service
         sudo -u $user XDG_RUNTIME_DIR="/run/user/$(id -u $user)" systemctl --user restart supershy-daemon.service
+    ;;
+    *"macos"*)
+        sudo echo '<?xml version="1.0" encoding="UTF-8"?>' >> $daemon
+        sudo echo '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">' >> $daemon
+        sudo echo '<plist version="1.0">' >> $daemon
+        sudo echo '<dict>' >> $daemon
+        sudo echo '<key>Label</key>' >> $daemon
+        sudo echo "<string>${app_id}</string>" >> $daemon
+        sudo echo '<key>ProgramArguments</key>' >> $daemon
+        sudo echo '<array>' >> $daemon
+        sudo echo '<string>supershyd</string>' >> $daemon
+        sudo echo '</array>' >> $daemon
+        sudo echo '<key>RunAtLoad</key>' >> $daemon
+        sudo echo '<true/>' >> $daemon
+        sudo echo '<key>KeepAlive</key>' >> $daemon
+        sudo echo '<true/>' >> $daemon
+        sudo echo '</dict>' >> $daemon
+        sudo echo '</plist>' >> $daemon
+
+        sudo chown -R $user $daemon
+
+        # run supershy daemon in background
+        launchctl enable user/501/${app_id}.plist
     ;;
 esac
