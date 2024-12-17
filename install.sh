66619c3780754d2130ed048eaba2a4847db0920b
Andrus Asumets <andrus.asumets@gmail.com>
2024-12-18 01:29:21 +0200

    v0.3.0

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# current user
 user=$1
 
 # install dependencies
@@ -16,18 +17,34 @@ fi
 
 # set platform target
 case $(uname -sm) in
-	"Darwin x86_64") target="macos-x86_64" ;;
-	"Darwin arm64") target="macos-arm64" ;;
-	"Linux aarch64") target="linux-arm64" ;;
-	*) target="linux-x86_64" ;;
+	"Darwin x86_64")
+        supershy_target="macos-x86_64"
+        tun2proxy_target="x86_64-apple-darwin"
+    ;;
+	"Darwin arm64")
+        supershy_target="macos-arm64"
+        tun2proxy_target="aarch64-apple-darwin"
+    ;;
+	"Linux aarch64")
+        supershy_target="linux-arm64"
+        tun2proxy_target="aarch64-unknown-linux-gnu"
+    ;;
+	*)
+        supershy_target="linux-x86_64"
+        tun2proxy_target="i686-unknown-linux-musl"
+    ;;
 esac
 
 version="$(curl -s https://version.supershy.org/)"
-uri="https://github.com/AndrusAsumets/supershy-client/releases/download/${version}/supershy-${target}.zip"
+supershy_uri="https://github.com/AndrusAsumets/supershy-client/releases/download/${version}/supershy-${supershy_target}.zip"
+tun2proxy_uri="https://github.com/tun2proxy/tun2proxy/releases/download/v0.6.6/tun2proxy-${tun2proxy_target}.zip"
+
 app_id="org.supershy.supershyd"
 daemon="/etc/systemd/user/supershy-daemon.service"
-data_dir="/home/${user}/.supershy-data"
-if [[ $target == *"macos"* ]]; then
+home_dir=$(getent passwd "$user" | cut -d: -f6)
+data_dir="${home_dir}/.supershy-data"
+usr_bin=/usr/bin
+if [[ $supershy_target == *"macos"* ]]; then
     launch_agents_dir="/Users/${user}/Library/LaunchAgents"
     sudo mkdir -p $launch_agents_dir
     sudo chown -R $user $launch_agents_dir
@@ -36,35 +53,54 @@ if [[ $target == *"macos"* ]]; then
 fi
 sudo mkdir -p $data_dir
 tmp_dir="/tmp"
-zip="$tmp_dir/supershy.zip"
-tmp_exe="$tmp_dir/supershyd"
-exe="$data_dir/supershyd"
 
-# remove old installation
-sudo rm -rf $exe
+# file paths
+supershy_zip="$tmp_dir/supershy.zip"
+tun2proxy_zip="$tmp_dir/tun2proxy.zip"
 
-# download the binary
-curl --fail --location --progress-bar --output "$zip" "$uri"
+supershy_tmp_exe="$tmp_dir/supershyd"
+tun2proxy_tmp_exe="$tmp_dir/tun2proxy-bin"
+
+supershy_exe="$data_dir/supershyd"
+tun2proxy_exe="$data_dir/tun2proxy-bin"
+
+# download
+curl --fail --location --progress-bar --output "$supershy_zip" "$supershy_uri"
+curl --fail --location --progress-bar --output "$tun2proxy_zip" "$tun2proxy_uri"
 
 # unzip
-unzip -d "$tmp_dir" -o "$zip"
+unzip -d "$tmp_dir" -o "$supershy_zip"
+unzip -d "$tmp_dir" -o "$tun2proxy_zip"
 
 # move to binaries
-sudo mv $tmp_exe $exe
+sudo mv $supershy_tmp_exe $supershy_exe
+sudo mv $tun2proxy_tmp_exe $tun2proxy_exe
 
-sudo chown -R $user $exe
+# make user the owner of the binaries
+sudo chown -R $user $supershy_exe
+sudo chown -R $user $tun2proxy_exe
+
+# link to system binaries
+supershy_link="$usr_bin"/supershyd
+tun2proxy_link="$usr_bin"/tun2proxy-bin
+
+sudo rm -f supershy_link
+sudo rm -f tun2proxy_link
+
+sudo ln -sf $supershy_exe $supershy_link
+sudo ln -sf $tun2proxy_exe $tun2proxy_link
 
 # remove old daemon service
 rm -f $daemon
 
 # create daemon servicea
-case $target in
+case $supershy_target in
     *"linux"*)
         sudo echo '[Unit]' >> $daemon
         sudo echo 'Description=supershyd' >> $daemon
 
         sudo echo '[Service]' >> $daemon
-        sudo echo "ExecStart=${exe}" >> $daemon
+        sudo echo 'ExecStart=supershyd' >> $daemon
         sudo echo 'Restart=always' >> $daemon
 
         sudo echo '[Install]' >> $daemon
@@ -96,7 +132,7 @@ case $target in
         sudo echo "<string>${app_id}</string>" >> $daemon
         sudo echo '<key>ProgramArguments</key>' >> $daemon
         sudo echo '<array>' >> $daemon
-        sudo echo "<string>${exe}</string>" >> $daemon
+        sudo echo "<string>${supershy_exe}</string>" >> $daemon
         sudo echo '</array>' >> $daemon
         sudo echo '<key>RunAtLoad</key>' >> $daemon
         sudo echo '<true/>' >> $daemon
