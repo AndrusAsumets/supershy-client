8dcd5f08c959c2b839003ed60015dbcf8ff109c6
Andrus Asumets <andrus.asumets@gmail.com>
2024-12-19 01:01:07 +0200

    uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,19 @@
-sudo pkill supershyd || true
-sudo pkill tun2proxy-bin || true
-rm -rf ~/.supershy-data
+#disable background services
 systemctl --user stop supershy-daemon.service || true
 systemctl --user disable supershy-daemon.service || true
 sudo rm /etc/systemd/user/supershy-daemon.service || true
+sudo -u $(whoami) launchctl unload $daemon &>/dev/null || true
+
+# stop application
+sudo pkill supershyd || true
+sudo pkill tun2proxy-bin || true
+
+# clean out application data
+rm -rf ~/.supershy-data
+
+# remove possible tun interface
 sudo ip link del tun0 || true
+
+# clear out dns
+sudo chattr -i "$(realpath /etc/resolv.conf)" || true
 sudo umount -f /etc/resolv.conf || true
-sudo sysctl net.ipv6.conf.all.disable_ipv6=0 || true
-sudo sysctl net.ipv6.conf.default.disable_ipv6=0 || true
